95eb5e18306889628a773d53692500b865b2c958
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-15 09:44:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 document.getElementById("user-form").addEventListener("submit", function (e) {
   e.preventDefault(); // prevent form submission
 
-  const firstName = document.getElementById("First Name").value.trim();
-  const lastName = document.getElementById("Last Name").value.trim();
-  const phone = document.getElementById("Phone Number").value.trim();
-  const email = document.getElementById("Email ID").value.trim();
+    const firstName = document.getElementsByName("First Name")[0].value.trim();
+  const lastName = document.getElementsByName("Last Name")[0].value.trim();
+  const phone = document.getElementsByName("Phone Number")[0].value.trim();
+  const email = document.getElementsByName("Email ID")[0].value.trim();
 
   const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phone} Email ID: ${email}`;
   alert(alertMessage);
